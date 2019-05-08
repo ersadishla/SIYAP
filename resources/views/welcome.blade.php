4c78103211a77467c4a76998b26e7f6bdaef6294
ersadishla <ersadishlahuddin@gmail.com>
2019-05-08 22:49:14 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,44 +10,43 @@
                 <div class="content-header">
                     <!-- Left Section -->
                     <div class="content-header-section">
-                        <a class="nav-link active" href="javascript:void(0)">
-                            <i class="fa fa-fw fa-home mr-5"></i>Beranda
-                        </a>
+                        <ul class="nav nav-tabs">
+                            <li class="nav-item">
+                                <a href="" class="nav-link active">
+                                <i class="fa fa-fw fa-home mr-5"></i>Beranda
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                     <!-- END Left Section -->
 
                     <!-- Right Section -->
                     <div class="content-header-section">
-                        <!-- User Dropdown -->
-                        <div class="btn-group" role="group">
-                            <a class="nav-link" href="javascript:void(0)">
-                                Profil
-                            </a>
-                            <a class="nav-link" href="javascript:void(0)">
-                                Data Anak
-                            </a>
-                            <a class="nav-link" href="javascript:void(0)">
-                                Berita
-                            </a>
-                            <a class="nav-link" href="javascript:void(0)">
-                                Kontak
-                            </a>
-                            <a class="nav-link" href="javascript:void(0)">
-                                Donasi
-                            </a>
-                            <div>
-                                
-                            </div>
-                            <a class="nav-link dropdown-toggle" href="javascript:void(0)">
-                                Lainnya
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs">
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Profil</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Data Anak</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Berita</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Kontak</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">Donasi</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Lainnya</a>
                                 <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="#">Link 1</a>
-                                    <a class="dropdown-item" href="#">Link 2</a>
-                                    <a class="dropdown-item" href="#">Link 3</a>
+                                    <a class="dropdown-item" href="">Inventaris</a>
+                                    <a class="dropdown-item" href="">Pengajuan Anak Panti</a>
                                 </div>
-                            </a>
-                        </div>
-                        <!-- END User Dropdown -->
+                            </li>
+                        </ul>
                     </div>
                     <!-- END Right Section -->
                 </div>
